3da623dbb4e0161bec41d7d1e8598806a82b2bb6
husnain <husnain@xs4arabia.com>
2023-12-14 15:32:43 +0300

    path name according to directory name for git refference

--- config_api.php
+++ config_api.php
@@ -6,6 +6,6 @@ if (file_exists(__DIR__ . '/_frontaccounting')) {
 	$rootPath = realpath(__DIR__ . '/../..');
 }
 
-define('API_ROOT', $rootPath . '/modules/api');
+define('API_ROOT', $rootPath . '/modules/FrontAccountingSimpleAPI');    //change to api if your api module directory name is api
 define('FA_ROOT', $rootPath);
 
